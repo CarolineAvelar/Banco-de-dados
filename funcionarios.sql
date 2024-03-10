809c11d8d3fd0925fc3df4fa6729d6d402b584a0
Carol <131722187+Carolzinha017@users.noreply.github.com>
2024-03-10 17:42:27 -0300

    Delete funcionarios.sql

--- funcionarios.sql
+++
@@ -1,8 +0,0 @@
--- Apaga o database/schema funcionarios
-drop database funcionarios;
-
--- Cria o database/schema funcionarios
-create database funcionario;
-
--- Define o database funcionario como default
-use funcionario;
