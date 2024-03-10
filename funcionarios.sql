a8134678d3356bc27c5de0d2c9d29ef7c6057ba9
Carol <131722187+Carolzinha017@users.noreply.github.com>
2024-03-10 17:41:16 -0300

    Add files via upload

---
+++ funcionarios.sql
@@ -0,0 +1,8 @@
+-- Apaga o database/schema funcionarios
+drop database funcionarios;
+
+-- Cria o database/schema funcionarios
+create database funcionario;
+
+-- Define o database funcionario como default
+use funcionario;
